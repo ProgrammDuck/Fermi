642835d0869fe80240c5f006893e56983ea69231
ProgrammDuck <iliakuzmin5555@gmail.com>
2025-04-29 01:02:53 +0300

    Update errors.py

--- Code/cogs/events/errors.py
+++ Code/cogs/events/errors.py
@@ -22,5 +22,29 @@ class main(commands.Cog):
             msg = eremb.copy()
             msg.description = error
             await ctx.reply(embed=msg)
+        elif isinstance(error, commands.MissingRequiredArgument):
+            msg = eremb.copy()
+            msg.description = 'Missing required argument. !help <command>'
+            await ctx.reply(embed=msg)
+        elif isinstance(error, commands.MissingPermissions):
+            msg = eremb.copy()
+            msg.description = 'You dont have permissions to eexecute this command.'
+            await ctx.reply(embed=msg)
+        elif isinstance(error, commands.BotMissingPermissions):
+            msg = eremb.copy()
+            msg.description = 'Bot doesnt have permissions.'
+            await ctx.reply(embed=msg)
+        elif isinstance(error, commands.CommandNotFound):
+            msg = eremb.copy()
+            msg.description = 'Command not found'
+            await ctx.reply(embed=msg)
+        elif isinstance(error, commands.MemberNotFound):
+            msg = eremb.copy()
+            msg.description = 'Member not found'
+            await ctx.reply(embed=msg)
+        
+            
+            
+            
 async def setup(bot):
     await bot.add_cog(main(bot))
